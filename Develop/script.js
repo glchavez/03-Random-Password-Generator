1bd96c562911d3ecc9681ab3c423b39bbd2d6dc1
Giovany <giochavez320@gmail.com>
2021-01-17 11:20:32 -0700

    Addition of working JS code

--- Develop/script.js
+++ Develop/script.js
@@ -11,4 +11,49 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", passwordCriteria);
+
+
+var lowerCaseLettersArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseLettersArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numericalCharArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialCharArr = ['!', '#', '$', '%', '&', '*', '+', ',', '-', '.', '/', ':', ';', '?', '@', '^', '_', '~'];
+
+var passwordLength = "";
+
+function passwordCriteria() {
+  passwordLength = prompt("Please choose a password length between 8 and 128 characters.")
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    writePassword();
+  } else {
+    alert("Incorrect value, please enter a value between 8 and 128.")
+  } return;
+}
+
+function generatePassword() {
+  var bigArray = [];
+  var finalPassword = "";
+
+
+    if (confirm("Would you like your password to include numbers?")) {
+      bigArray = bigArray.concat(numericalCharArr)
+    }
+
+    if (confirm("Would you like your password to include lowercase characters?")) {
+      bigArray = bigArray.concat(lowerCaseLettersArr)
+    }
+
+    if (confirm("Would you like your password to include uppercase characters?")) {
+      bigArray = bigArray.concat(upperCaseLettersArr)
+    }
+
+    if (confirm("Would you like your password to include special characters?")) {
+      bigArray = bigArray.concat(specialCharArr)
+    }
+
+    for (i = 0; i < passwordLength; i++) {
+    finalPassword = finalPassword + bigArray[Math.floor(Math.random() * bigArray.length)];
+  }
+
+  return finalPassword
+} 
